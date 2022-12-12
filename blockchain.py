70a1ef0b70fc5f63083fccd6229c318da07e79ed
NiloySarkar10 <nlysrkr10@gmail.com>
2022-12-12 20:57:43 +0530

    Update blockchain.py
    Added genesis block (first dummy block for the blockchain) and added the first basic logic for the mining function to mine new blocks to the blockchain

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,11 @@
 # initializing empty blockchain value
+genesis_block = {           # dummy block at the start of the blockchain
+    'previous_hash': '',
+    'index': 0,
+    'transactions': []
+} 
 blockchain = []
+blockchain.append(genesis_block)
 open_transactions = []
 owner = 'Niloy'
 
@@ -28,7 +34,13 @@ def add_transaction(recipient, sender=owner, amount=1.0):
 
 
 def mine_block():
-    pass
+    last_block = blockchain[-1]
+    block = {
+        'previous_hash': 'XYZ',
+        'index': len(blockchain),
+        'transactions': open_transactions
+    }
+    blockchain.append(block)
 
 
 def get_transaction_value():
@@ -106,52 +118,4 @@ else:
 
 
 print('OVER.')
-
-
-
-
-
-
-
-
-
-
-
-
-
-def add_value(transaction_amount, last_transaction=[1]):
-    ''' Adds value to the blockchain 
-    
-    Arguments:
-        :transaction_amount {int} -- amount to add to the blockchain
-        :last_transaction {list} -- list of the last transactions in the blockchain (default = [1])
-    '''
-    blockchain.append([last_transaction, transaction_amount])
-
-# Get user data
-def get_user_data():
-    ''' Returns the user data entered by the user '''
-    return float(input('Please enter the amount: '))
-
-
-def get_user_input_and_add_to_blockchain(last_transaction_value=[1]):
-    ''' Returns the user input and adds it to the blockchain
-    
-    Arguments:
-        :last_transaction_value {int} -- indicator if the blockchain is empty or not
-        (default = [1] indicates it is empty)'''
-    tx_amount = get_user_data()
-    if last_transaction_value == [1]:
-        add_value(tx_amount)
-    else:
-        add_value(tx_amount, last_transaction_value)
-
-
-get_user_input_and_add_to_blockchain()
-
-while True:
-    get_user_input_and_add_to_blockchain(get_last_blockchain_value())
-    for value in blockchain:
-        print(value)
-
 
